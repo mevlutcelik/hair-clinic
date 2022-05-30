160aeefef27fa98fd6f77ec10a9865cf51cf7b72
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-05-30 12:02:11 +0300

    HTML template of plan cards was prepared.

---
+++ resources/views/components/plan-cards.blade.php
@@ -0,0 +1,47 @@
+<div class="plan-cards">
+    <div class="plan-card">
+        <div class="plan-num">01</div>
+        <div class="plan-title">Before the Hair Transplant Operation</div>
+        <div class="plan-dvider"></div>
+        <ul>
+            <li>Online Free Hair Consultation</li>
+            <li>7/24 Customer Service</li>
+            <li>Booking Airport and Hotel</li>
+            <li>Booking a Surgical Date</li>
+        </ul>
+        <a href="#!">
+            <span>Learn More</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z"/></svg>
+        </a>
+    </div>
+    <div class="plan-card">
+        <div class="plan-num">02</div>
+        <div class="plan-title">During the Hair Transplant Operation</div>
+        <div class="plan-dvider"></div>
+        <ul>
+            <li>Blood Tests</li>
+            <li>Painless Anaesthesia</li>
+            <li>You Hair transplant operation</li>
+            <li>Medication and Hair Care Kit</li>
+        </ul>
+        <a href="#!">
+            <span>Learn More</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z"/></svg>
+        </a>
+    </div>
+    <div class="plan-card">
+        <div class="plan-num">03</div>
+        <div class="plan-title">After the Hair Transplant Operation</div>
+        <div class="plan-dvider"></div>
+        <ul>
+            <li>VIP Airport Transfer</li>
+            <li>7/24 Customer Service</li>
+            <li>Lifetime follow-up</li>
+            <li>30-year Warranty</li>
+        </ul>
+        <a href="#!">
+            <span>Learn More</span>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6l-160 160C272.4 444.9 264.2 448 256 448s-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L338.8 288H32C14.33 288 .0016 273.7 .0016 256S14.33 224 32 224h306.8l-105.4-105.4c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l160 160C451.1 245.9 451.1 266.1 438.6 278.6z"/></svg>
+        </a>
+    </div>
+</div>
--- resources/views/pages/home/index.blade.php
+++ resources/views/pages/home/index.blade.php
@@ -3,4 +3,5 @@
 @section('content')
     @include('components.navbar')
     @include('components.header-slide')
+    @include('components.plan-cards')
 @endsection
